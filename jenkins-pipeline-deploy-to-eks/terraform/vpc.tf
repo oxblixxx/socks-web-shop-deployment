4b924dd213c89fbe00432f99352755b76bc9d712
oxblixxx <phamust1111@gmail.com>
2023-03-18 08:40:04 +0100

    remove private subnets

--- jenkins-pipeline-deploy-to-eks/terraform/vpc.tf
+++ jenkins-pipeline-deploy-to-eks/terraform/vpc.tf
@@ -4,7 +4,7 @@ module "myapp-vpc" {
   version         = "3.19.0"
   name            = "myapp-vpc"
   cidr            = var.vpc_cidr_block
-  private_subnets = var.private_subnet_cidr_blocks
+  # private_subnets = var.private_subnet_cidr_blocks
   public_subnets  = var.public_subnet_cidr_blocks
   azs             = data.aws_availability_zones.azs.names
 
@@ -21,10 +21,10 @@ module "myapp-vpc" {
     "kubernetes.io/role/elb"                  = 1
   }
 
-  private_subnet_tags = {
-    "kubernetes.io/cluster/socks-web-shop" = "shared"
-    "kubernetes.io/role/internal-elb"         = 1
-  }
+  # private_subnet_tags = {
+  #   "kubernetes.io/cluster/socks-web-shop" = "shared"
+  #   "kubernetes.io/role/internal-elb"         = 1
+  # }
 }
 
 
