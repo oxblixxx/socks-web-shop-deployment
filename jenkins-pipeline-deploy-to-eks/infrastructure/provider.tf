ffab6f2348084c0154828cd1420400d957563074
oxblixxx <phamust1111@gmail.com>
2023-03-18 21:20:00 +0100

    comment provider dot tf

--- jenkins-pipeline-deploy-to-eks/infrastructure/provider.tf
+++ jenkins-pipeline-deploy-to-eks/infrastructure/provider.tf
@@ -4,44 +4,44 @@ provider "aws" {
 }
 
 
-data "aws_eks_cluster" "eks-cluster" {
-  name = "cluster_name"
-}
+# data "aws_eks_cluster" "eks-cluster" {
+#   name = "cluster_name"
+# }
 
-terraform {
-  required_providers {
-    kubectl = {
-      source = "gavinbunney/kubectl"
-      version = "1.14.0"
-    }
-  }
-}
-
-
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks-cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
-  # version          = "2.16.1"
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks-cluster.name]
-    command     = "aws"
-  }
-}
-
-# Kubectl provider configuration
+# terraform {
+#   required_providers {
+#     kubectl = {
+#       source = "gavinbunney/kubectl"
+#       version = "1.14.0"
+#     }
+#   }
+# }
 
 
 
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.eks-cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
+#   # version          = "2.16.1"
 
-provider "kubectl" {
-  host                   = data.aws_eks_cluster.eks-cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks-cluster.name]
-    command     = "aws"
-  }
-}
+#   exec {
+#     api_version = "client.authentication.k8s.io/v1alpha1"
+#     args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks-cluster.name]
+#     command     = "aws"
+#   }
+# }
+
+# # Kubectl provider configuration
+
+
+
+
+# provider "kubectl" {
+#   host                   = data.aws_eks_cluster.eks-cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
+#   exec {
+#     api_version = "client.authentication.k8s.io/v1alpha1"
+#     args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks-cluster.name]
+#     command     = "aws"
+#   }
+# }
