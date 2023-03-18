5dcaa5dce9eaa4690e2cdb9641a393b88fc6c6d2
oxblixxx <phamust1111@gmail.com>
2023-03-18 10:25:42 +0100

    added file

---
+++ jenkins-pipeline-deploy-to-eks/terraform/socks-service.tf
@@ -0,0 +1,46 @@
+# Create kubernetes Name space for socks shop app
+
+
+
+resource "kubernetes_namespace" "kube-namespace-socks" {
+  metadata {
+    name = "sock-shop"
+  }
+}
+
+# Create kubectl deployment for socks app
+
+data "kubectl_file_documents" "docs" {
+    content = file("eks-manifest.yaml")
+}
+
+resource "kubectl_manifest" "kube-deployment-socks" {
+    for_each  = data.kubectl_file_documents.docs.manifests
+    yaml_body = each.value
+}
+
+# Create separate kubernetes service for socks shop frontend
+
+resource "kubernetes_service" "kube-service-socks" {
+  metadata {
+    name      = "front-end"
+    namespace = kubernetes_namespace.kube-namespace-socks.id
+    annotations = {
+      "prometheus.io/scrape" = "true"
+    }
+    labels = {
+      name = "front-end"
+    }
+  }
+  spec {
+    selector = {
+      name = "front-end"
+    }
+    port {
+      name = "metrics"
+      port        = 80
+      target_port = 8079
+    }
+    type = "LoadBalancer"
+  }
+}
