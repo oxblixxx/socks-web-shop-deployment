5f5381d9ac7a3d020546f138c667b4041fc1ccda
oxblixxx <phamust1111@gmail.com>
2023-03-18 10:19:58 +0100

    add files

--- jenkins-pipeline-deploy-to-eks/terraform/provider.tf
+++ jenkins-pipeline-deploy-to-eks/terraform/provider.tf
@@ -2,3 +2,44 @@ provider "aws" {
   region = "us-west-2"
   # other provider settings here
 }
+
+
+data "aws_eks_cluster" "eks-cluster" {
+  name = module.eks_cluster.socks-web-shop
+}
+
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.18.1"
+    }
+  }
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks-cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
+  # version          = "2.16.1"
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks-cluster.name]
+    command     = "aws"
+  }
+}
+
+# Kubectl provider configuration
+
+
+
+
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.eks-cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks-cluster.name]
+    command     = "aws"
+  }
+}
--- jenkins-pipeline-deploy-to-eks/terraform/vpc.tf
+++ jenkins-pipeline-deploy-to-eks/terraform/vpc.tf
@@ -4,12 +4,12 @@ module "myapp-vpc" {
   version         = "3.19.0"
   name            = "myapp-vpc"
   cidr            = var.vpc_cidr_block
-  # private_subnets = var.private_subnet_cidr_blocks
+  private_subnets = var.private_subnet_cidr_blocks
   public_subnets  = var.public_subnet_cidr_blocks
   azs             = data.aws_availability_zones.azs.names
 
-# enable_nat_gateway   = true
-#  single_nat_gateway   = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
 
   tags = {
@@ -21,10 +21,10 @@ module "myapp-vpc" {
     "kubernetes.io/role/elb"                  = 1
   }
 
-  # private_subnet_tags = {
-  #   "kubernetes.io/cluster/socks-web-shop" = "shared"
-  #   "kubernetes.io/role/internal-elb"         = 1
-  # }
+  private_subnet_tags = {
+    "kubernetes.io/cluster/socks-web-shop" = "shared"
+    "kubernetes.io/role/internal-elb"         = 1
+  }
 }
 
 
