432b7eec5a69ac64051e571d43aa19f78f9309e0
oxblixxx <phamust1111@gmail.com>
2023-03-19 21:21:49 +0100

    added load config

--- jenkins-pipeline-deploy-to-eks/deployment/provider.tf
+++ jenkins-pipeline-deploy-to-eks/deployment/provider.tf
@@ -37,6 +37,7 @@ provider "kubernetes" {
 
 
 provider "kubectl" {
+  load_config_file  = false
   host                   = data.aws_eks_cluster.eks-cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
   exec {
