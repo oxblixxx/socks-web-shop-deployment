36f044290c6b5091ac9856fad26dc3e70dc305e4
oxblixxx <phamust1111@gmail.com>
2023-03-19 22:46:13 +0100

    moved all to a folder

--- jenkins-pipeline-deploy-to-eks/deployment/backend.tf
+++ jenkins-pipeline-deploy-to-eks/deployment/backend.tf
@@ -1,7 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket = "new-state-file-west-2-516"
-    key    = "deployment"
-   region = "us-west-2"
-  }
-}
+/
--- jenkins-pipeline-deploy-to-eks/deployment/vpc.tf
+++ jenkins-pipeline-deploy-to-eks/deployment/vpc.tf
@@ -1,8 +1,8 @@
 data "aws_availability_zones" "azs" {}
-module "myapp-vpc" {
+module "socks-vpc" {
   source          = "terraform-aws-modules/vpc/aws"
   version         = "3.19.0"
-  name            = "myapp-vpc"
+  name            = "socks-vpc"
   cidr            = var.vpc_cidr_block
   private_subnets = var.private_subnet_cidr_blocks
   public_subnets  = var.public_subnet_cidr_blocks
