9bc6466006c23d96d614940994e9dff524ea8b99
Mutapha Pelumi Babatunde <97739416+oxblixxx@users.noreply.github.com>
2023-03-21 16:44:25 +0100

    add token

---
+++ jenkins-pipeline-deploy-to-eks/monitoring/provider.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+  region = "us-west-2"
+  # other provider settings here
+}
+
+
+data "aws_eks_cluster" "eks-cluster" {
+  name = "socks-web-shop"
+}
+
+terraform {
+  required_providers {
+    kubectl = {
+      source = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
+  }
+}
+
+
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks-cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
+  # version          = "2.16.1"
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks-cluster.name]
+    command     = "aws"
+  }
+}
+
+# Kubectl provider configuration
+
+
+
+
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.eks-cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks-cluster.name]
+    command     = "aws"
+  }
+}
+
+# Heml provider configuration
+
+provider "helm" {
+  kubernetes {
+  host                   = data.aws_eks_cluster.eks-cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
+  token = data.aws_eks_cluster_auth.eks-token.token
+
+  }
+ 
