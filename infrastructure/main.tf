1afd888236cc16ddbf3d5fec045f7d34abf45fc0
Mutapha Pelumi Babatunde <phamust1111@gmail.com>
2024-03-17 09:40:22 +0000

    modified source name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,5 @@
 module "Eks infrastructure" {
-    source = "../jenkins-pipeline-modules/infrastructure"
+    source = "../socks-infra-modules/infrastructure"
     vpc_cidr_block = "10.10.0.0/28"
     private_subnet_cidr_blocks =  "10.10.0.0/29"
     public_subnet_cidr_blocks =   "10.10.0.16/29"
