b8dc1c1c7866eaad0e482a787e8f6511b13ea009
oxblixxx <phamust1111@gmail.com>
2024-03-18 08:17:33 +0100

    modified main.tf

--- deployment/main.tf
+++ deployment/main.tf
@@ -1,10 +1,3 @@
 module "eks-infrastructure" {
     source = "../socks-infra-pipeline-modules/deployment"
-    AWS_ACCESS_KEY_ID =  var.AWS_ACCESS_KEY_ID
-    AWS_SECRET_KEY =  var.AWS_SECRET_KEY
-}
-
-provider "aws" {
-    access_key = var.AWS_ACCESS_KEY_ID
-    secret_key = var.AWS_SECRET_KEY
 }
