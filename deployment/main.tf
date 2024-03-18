fb2f4453d2f7652da1037b845764ee78eb2ae7b4
oxblixxx <phamust1111@gmail.com>
2024-03-18 06:46:23 +0100

    modified config

--- deployment/main.tf
+++ deployment/main.tf
@@ -1,3 +1,8 @@
 module "eks-infrastructure" {
     source = "../socks-infra-pipeline-modules/deployment"
 }
+
+provider "aws" {
+    access_key = vaar.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_KEY_ID
+}
