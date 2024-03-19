89279177a6235b900c318a0716375c0145d51a73
Mutapha Pelumi Babatunde <97739416+oxblixxx@users.noreply.github.com>
2024-03-19 05:00:43 +0100

    Update provider.tf

--- socks-infra-pipeline-modules/deployment/provider.tf
+++ socks-infra-pipeline-modules/deployment/provider.tf
@@ -12,17 +12,32 @@ terraform {
   }
 }
 
+data "aws_eks_cluster" "eks_cluster" {
+    name = module.eks.cluster_name
+}
 
+data "aws_eks_cluster_auth" "cluster_auth" {
+    name = module.eks.cluster_name
+}
 
-#provider "kubernetes" {
-#  host                   = data.aws_eks_cluster.eks-cluster.endpoint
-#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks-cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
 
-#  exec {
-#    api_version = "client.authentication.k8s.io/v1beta1"
-#    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks-cluster.name]
-#    command     = "aws"
-#  }
-#}
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks_cluster.name]
+    command     = "aws"
+  }
+}
 
 # Kubectl provider configuration
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.eks-_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks_cluster.name]
+    command     = "aws"
+  }
+}
