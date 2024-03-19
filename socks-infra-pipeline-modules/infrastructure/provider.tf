ad479bf8a8cc43a908e20a25dc714e5c8781a630
Mutapha Pelumi Babatunde <97739416+oxblixxx@users.noreply.github.com>
2024-03-19 04:14:02 +0100

    Update provider.tf

--- socks-infra-pipeline-modules/infrastructure/provider.tf
+++ socks-infra-pipeline-modules/infrastructure/provider.tf
@@ -9,12 +9,12 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
   load_config_file = false
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks_cluster.id]
-    command     = "aws"
-  }
+  token = data.aws_aws_eks_cluster_auth.cluster_auth
+  #exec {
+  #  api_version = "client.authentication.k8s.io/v1beta1"
+  #  args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks_cluster.id]
+  #  command     = "aws"
+  #}
 }
 
 
